599e94afd28ee709b9236f98df0f3a52fcd69295
israelsanttana <israelsantanasantos7@gmail.com>
2023-04-16 20:20:52 -0300

    feat: adiciona função que lança n o localStorage

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-function startModal(modal){
+
+function startModal(modal) {
     const getModal = document.getElementById('modal');
     getModal.classList.add('active');
 }
 
-function closeModal(modal){
+function closeModal(modal) {
     const getModal = document.getElementById('modal');
     getModal.classList.remove('active');
 }
 
 const openModal = document.querySelector('#client');
-client.addEventListener('click', function(){
+client.addEventListener('click', function () {
     startModal('modal');
 });
 
 const noneModal = document.querySelector('#modalClose');
-modalClose.addEventListener('click', function(){
+modalClose.addEventListener('click', function () {
     closeModal('modal');
-})
+});
+
+const tempClient = {
+    nome: 'Israel',
+    email: 'israelsantana@gmail.com',
+    celular: '(41) 98904-2299',
+    cidade: 'Curitiba'
+
+}
+
+const createClient = (client) => {
+    localStorage.setItem("db_client", JSON.stringify(client));
+
+}
 
 
 
