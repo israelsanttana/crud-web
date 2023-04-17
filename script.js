5c96db214e682665f3e473675912e771fca31612
israelsanttana <israelsantanasantos7@gmail.com>
2023-04-17 19:37:19 -0300

    fix: melhora função para criar client

--- script.js
+++ script.js
@@ -27,8 +27,15 @@ const tempClient = {
 
 }
 
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
+
+// CRUD CREATE 
 const createClient = (client) => {
-    localStorage.setItem("db_client", JSON.stringify(client));
+    const dbClient = getLocalStorage();
+    dbClient.push(client);
+    setLocalStorage(dbClient)
+
 
 }
 
